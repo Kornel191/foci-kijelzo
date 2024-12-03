40cf2040e165dc832a8a163773d564dcf9260bfd
Ayame-3244 <114740782+Ayame-3244@users.noreply.github.com>
2024-12-03 19:27:39 +0100

    Visszaszámláló (óra)
    Az időszál (Óra megcsinálása)

--- js.js
+++ js.js
@@ -1,19 +1,31 @@
-let csapat1 = document.getElementById("csapat1").value;
+/*let csapat1 = document.getElementById("csapat1").value;
 let csapat2 = document.getElementById("csapat2").value;
 
 if (csapat1 != "" && csapat2 != "")
 {
     document.getElementById("csapat1NevKi").innerHTML = csapat1;
     document.getElementById("csapat2NevKi").innerHTML = csapat2;
-}
-/*
-let ido = 60;
+}*/
 
+let ido = 60 * 10;
 let idoszal = setInterval(idok, 1000);
 
 function idok() {
-    ido--;
     let ora = document.getElementById("timer");
-
+    if(ido > 0)
+    {
+        ido--;
+    }
+    else 
+    {
+        clearInterval(idok);
+    }
+    let m = Math.floor(ido / 60);
+    let s = ido % 60;
+    ora.innerHTML = `${m}:${s}`;
 }
-    */
+
+
+
+
+
