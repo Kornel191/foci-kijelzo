eb36a565d8acff01ea04448b38ac5ce90eaa88c0
Ayame-3244 <114740782+Ayame-3244@users.noreply.github.com>
2024-12-03 17:46:45 +0100

    Próbálkozás a js-sel
    Megpróbáltam megcsinálni pár dolgot, nem nagyon jött össze. Pár dolgot módosítottam/hozzáírtam a html-hez és css-hez.

---
+++ js.js
@@ -0,0 +1,19 @@
+let csapat1 = document.getElementById("csapat1").value;
+let csapat2 = document.getElementById("csapat2").value;
+
+if (csapat1 != "" && csapat2 != "")
+{
+    document.getElementById("csapat1NevKi").innerHTML = csapat1;
+    document.getElementById("csapat2NevKi").innerHTML = csapat2;
+}
+/*
+let ido = 60;
+
+let idoszal = setInterval(idok, 1000);
+
+function idok() {
+    ido--;
+    let ora = document.getElementById("timer");
+
+}
+    */
