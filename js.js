35dae5347564f45f0b21bc155e6a693c1c374f8b
Ayame-3244 <114740782+Ayame-3244@users.noreply.github.com>
2024-12-04 10:24:21 +0100

    Stop gomb
    stop gomb alapállása legyen disabled (kimaradt)

--- js.js
+++ js.js
@@ -1,7 +1,8 @@
 let ido = 60 * 10;
 let idoszal;
 let startGomb = document.getElementById("start");
-let stopGomb = document.getElementById("stop");   
+let stopGomb = document.getElementById("stop"); 
+stopGomb.disabled = true;   
 function nevek() {
     let csapat1 = document.getElementById("csapat1").value;
     let csapat2 = document.getElementById("csapat2").value;
