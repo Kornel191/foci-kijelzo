141ce635b7c7a24b947ab1fcaf40a2e74ed9bbf6
Ayame-3244 <114740782+Ayame-3244@users.noreply.github.com>
2024-12-05 08:42:56 +0100

    Plusz idő megcsinálása (elkezdés)
    Még nincs kész. Nem tudtam befejezni.

--- js.js
+++ js.js
@@ -2,7 +2,11 @@ let ido = 60 * 10;
 let idoszal;
 let startGomb = document.getElementById("start");
 let stopGomb = document.getElementById("stop"); 
+let pluszidoszal;
+let startGombPlusz = document.getElementById("startplusz");
+let stopGombPlusz = document.getElementById("stopplusz"); 
 stopGomb.disabled = true;   
+
 function nevek() {
     let csapat1 = document.getElementById("csapat1").value;
     let csapat2 = document.getElementById("csapat2").value;
@@ -35,31 +39,66 @@ function idok() {
     else 
     {
         clearInterval(idok);
-        hozzaadasContainer.style.display = 'block'; 
-        alert('Lejárt az idő!');
+        /*hozzaadasContainer.style.display = 'block'; 
+        alert('Lejárt az idő!');*/
     }
 
     let m = Math.floor(ido / 60);
     let s = ido % 60;
     ora.innerHTML = `${m}:${s}`;
 }
-
+/*
 function pluszido() 
 {
     let plusz = parseInt(document.getElementById("plusszInput").value) * 60;
-}
+
+}*/
+
+
+
+
+let pperc = parseInt(document.getElementById("plusszInput")).value;
+let pluszido = pperc * 60;
+
 function idoStartPlussz() {
-    idoszal = setInterval(idok, 1000);
-    startGomb.disabled = true;
-    stopGomb.disabled = false; 
+    if(pperc > 0 )
+    {
+        pluszidoszal = setInterval(pluszidok, 1000);
+        startGombPlusz.disabled = true;
+        stopGombPlusz.disabled = false; 
+    }
 }
 function stopPlussz() {
-    clearInterval(idoszal);
-    startGomb.disabled = false;
-    stopGomb.disabled = true; 
+    clearInterval(pluszidoszal);
+    startGombPlusz.disabled = false;
+    stopGombPlusz.disabled = true; 
 }
 
-hozzaadasButton.addEventListener('click', pluszido);
+function pluszidok() {
+    
+    let pluszora = document.getElementById("plusztimer");
+    if(pluszido > 0)
+    {
+        pluszido--;
+    }
+    else 
+    {
+        clearInterval(pluszidok);
+        /*hozzaadasContainer.style.display = 'block'; 
+        alert('Lejárt az idő!');*/
+    }
+
+    let pluszm = Math.floor(ido / 60);
+    let pluszs = ido % 60;
+    pluszora.innerHTML = `${pluszm}:${pluszs}`;
+}
+
+
+
+
+
+
+//hozzaadasButton.addEventListener('click', pluszido);
 
 
 
