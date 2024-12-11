3119eaf7e4ec52ff444a2cf0ce32dc075f06aa42
Ayame-3244 <114740782+Ayame-3244@users.noreply.github.com>
2024-12-11 09:36:58 +0100

    Pontozás
    Mindkét csapat gól számlálóját megcsináltam

--- js.js
+++ js.js
@@ -63,27 +63,57 @@ function plusido()
 let pont1 = 0
 let pont2 = 0
 
-let cs2Pont = parseInt(document.getElementById("csapat2pont").value)
+let cs2Pont = document.getElementById("csapat2pont")
 let cs1Pont = document.getElementById("csapat1pont")
 
-function pontok1plusz ()
+let minus1Gomb = document.getElementById("cs1minuspont")
+let minus2Gomb = document.getElementById("cs2minuspont")
+function pontok1plusz()
 {
     pont1++;
     cs1Pont.innerHTML = pont1
-    
+    if (pont1 > 0)
+        minus1Gomb.disabled = false
 }
 
-function pontok1minus ()
-{
-    if (pont1 == 0 )
-    {
 
+function pontok1minus()
+{
+    if (pont1 == 0)
+    {
+        minus1Gomb.disabled = true
     }
     else
     {
         pont1--;
         let cs1Pont = document.getElementById("csapat1pont")
         cs1Pont.innerHTML = pont1
+    }
+
+}
+
+
+function pontok2plusz()
+{
+    pont2++;
+    cs2Pont.innerHTML = pont2
+    if (pont2 > 0)
+        minus2Gomb.disabled = false
+    
+}
+
+
+function pontok2minus()
+{
+    if (pont2 == 0 )
+    {
+        minus2Gomb.disabled = true
+    }
+    else
+    {
+        pont2--;
+        let cs2Pont = document.getElementById("csapat2pont")
+        cs2Pont.innerHTML = pont2
     }
 
 }
