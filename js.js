fd15267443000e1ec1a8ab64d1a65f6ebbc2bb62
Ayame-3244 <114740782+Ayame-3244@users.noreply.github.com>
2024-12-11 09:59:55 +0100

    Csapat nevek
    Csapat nevek átírása az egyik HTML-ből a másikba

--- js.js
+++ js.js
@@ -118,6 +118,21 @@ function pontok2minus()
 
 }
 
+function atiras()
+{
+    localStorage.setItem("nev1",document.getElementById("csapat1").value)
+    localStorage.setItem("nev2",document.getElementById("csapat2").value)
+}
+
+const ki1 = localStorage.getItem("nev1")
+const ki2 = localStorage.getItem("nev2")
+document.getElementById("csapat1NevKi").innerHTML = ki1;
+document.getElementById("csapat2NevKi").innerHTML = ki2;
+
+
+
+
+
 /*
 function pluszido() 
 {
